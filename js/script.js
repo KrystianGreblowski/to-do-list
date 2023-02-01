11e4a57cef7f987f057d9197f602bad7428e35e9
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-02-01 19:46:55 +0100

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -26,23 +26,9 @@
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
-    }
+    };
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-                <button class="js-done">Zrobione?</button>
-                <button class="js-remove">Usuń</button>
-                ${task.content}
-            </li>
-            `;
-        };
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -58,6 +44,24 @@
                 toggleTaskDone(index);
             });
         });
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                <button class="js-done">Zrobione?</button>
+                <button class="js-remove">Usuń</button>
+                ${task.content}
+            </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+   
+        bindEvents();
     };
 
     const onFormSubmit = (event) => {
