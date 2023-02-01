719d46538b737675e964e6812a1c54a7743b27b0
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-02-01 19:40:06 +0100

    toggle task done

--- js/script.js
+++ js/script.js
@@ -18,10 +18,15 @@
         render();
     };
 
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
         render();
     };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
 
     const render = () => {
         let htmlString = "";
@@ -29,6 +34,7 @@
         for (const task of tasks) {
             htmlString += `
             <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                <button class="js-done">Zrobione?</button>
                 <button class="js-remove">Usuń</button>
                 ${task.content}
             </li>
@@ -42,6 +48,14 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
